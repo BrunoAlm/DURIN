35e38cad69bd8fd15a6957561756ab0a894991f6
Bruno Almeida <brunoalmeidadem@gmail.com>
2023-05-24 13:21:50 -0300

    update path

--- contadores_zebra/src/functions/gravar_contador.py
+++ contadores_zebra/src/functions/gravar_contador.py
@@ -1,7 +1,7 @@
-# grava uma lista de contadores em um arquivo txt
 
+# Função para gravar o contador no arquivo contador.txt
 def gravar_contador(contadores):
     # Abre o arquivo em modo de escrita (w)
-    with open('contador.txt', 'w', encoding='utf-8') as arquivo:
+    with open('generated/contador.txt', 'w', encoding='utf-8') as arquivo:
         # Escrever várias linhas no arquivo
         arquivo.writelines(contadores)
