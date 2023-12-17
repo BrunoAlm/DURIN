bde4307d906f4aafedbe2f4a7684d4f79c59ad8b
Bruno Almeida de Matos <brunoalmeidadem@gmail.com>
2023-12-17 19:07:58 -0300

    Create printers_controller.dart

---
+++ contadores/agora-vai/app/lib/src/navigation/features/printers/printers_controller.dart
@@ -0,0 +1,35 @@
+import 'dart:developer';
+import 'package:app/src/navigation/features/printers/printers_entity.dart';
+import 'package:app/src/navigation/features/printers/printers_repository.dart';
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+
+class PrintersController extends ChangeNotifier {
+  final PrintersRepository _printersRepository = PrintersRepositoryV1();
+  final printers = ValueNotifier(<PrintersEntity>[]);
+  List<bool> _selected = [];
+
+  List<bool> get isSelected => _selected;
+
+  set selectPrinter(int index) {
+    _selected[index] = !_selected[index];
+    notifyListeners();
+  }
+
+  void init() async {
+    try {
+      printers.value = await _printersRepository.list();
+      _selected =
+          List<bool>.generate(printers.value.length, (int index) => false);
+    } on DioException catch (e) {
+      log(
+        'Response: ${e.response}\n'
+        'Error: ${e.error}\n'
+        'Message: ${e.message}\n'
+        'StackTrace: ${e.stackTrace}',
+        name: 'printersRepository.list',
+      );
+      print(e.message);
+    }
+  }
+}
