da264ac7acec9aeea21a72183d9018b7b1848298
Bruno Almeida de Matos <brunoalmeidadem@gmail.com>
2023-12-17 19:07:51 -0300

    add cors

--- contadores/agora-vai/api/api/__init__.py
+++ contadores/agora-vai/api/api/__init__.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
 from .routes import router as routes_router
+from fastapi.middleware.cors import CORSMiddleware
 from . import database
 
 app = FastAPI()
+
+# Configuração das políticas de CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Isso permite que todos os origens acessem a API
+    allow_credentials=True,
+    allow_methods=["*"],  # Isso permite todos os métodos HTTP (GET, POST, etc.)
+    allow_headers=["*"],  # Isso permite todos os headers
+)
 
 app.include_router(routes_router)
 
