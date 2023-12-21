497c668b9f39fde4c1816f3f14ce8f797b8f9bc8
Bruno Almeida de Matos <brunoalmeidadem@gmail.com>
2023-12-21 17:43:14 -0300

    add updatePrinter

--- contadores/agora-vai/app/lib/src/navigation/features/printers/printers_repository.dart
+++ contadores/agora-vai/app/lib/src/navigation/features/printers/printers_repository.dart
@@ -3,23 +3,33 @@ import 'package:app/src/navigation/features/printers/printers_entity.dart';
 
 abstract interface class PrintersRepository {
   Future<List<PrintersEntity>> list();
-  Future<void> updatePrinters(Map<String, dynamic> body);
+  Future<void> updateCounters(Map<String, dynamic> body);
+  Future<void> updatePrinter(Map<String, dynamic> body);
 }
 
 class PrintersRepositoryV1 implements PrintersRepository {
   final ApiService _apiService;
   PrintersRepositoryV1(this._apiService);
-  
+
   @override
   Future<List<PrintersEntity>> list() async {
     var result = await _apiService.get(path: "/api/impressoras");
     return (result as List).map((e) => PrintersEntity.fromMap(e)).toList();
   }
-  
+
   @override
-  Future<void> updatePrinters(Map<String, dynamic> body) async {
+  Future<void> updateCounters(Map<String, dynamic> body) async {
     try {
       await _apiService.post(path: "/api/impressoras", body: body);
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  @override
+  Future<void> updatePrinter(Map<String, dynamic> body) async {
+    try {
+      await _apiService.put(path: "/api/impressoras", body: body);
     } catch (e) {
       rethrow;
     }
