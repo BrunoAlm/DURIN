c38060903afe8909d6d1b036380a7850cb2e3bee
Bruno Almeida de Matos <brunoalmeidadem@gmail.com>
2023-12-21 17:45:05 -0300

    Update printers_cards.dart

--- contadores/agora-vai/app/lib/src/navigation/features/printers/components/printers_cards.dart
+++ contadores/agora-vai/app/lib/src/navigation/features/printers/components/printers_cards.dart
@@ -10,6 +10,48 @@ class PrintersCards extends StatefulWidget {
 }
 
 class _PrintersCardsState extends State<PrintersCards> {
+  void editPrinter(PrintersEntity printer) {
+    showDialog(
+      context: context,
+      builder: (context) => SimpleDialog(
+        title: const Text('Editar impressora'),
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(18.0),
+            child: Form(
+              child: TextFormField(
+                decoration: const InputDecoration(
+                  enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(20))),
+                  focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(20))),
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 18.0, vertical: 8),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                ElevatedButton(
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                    child: const Text('Confirmar')),
+                ElevatedButton(
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                    child: const Text('Cancelar')),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     var printers = widget.printers;
@@ -41,7 +83,7 @@ class _PrintersCardsState extends State<PrintersCards> {
                       const SizedBox(width: 20),
                       CircleAvatar(
                         child: IconButton(
-                          onPressed: () {},
+                          onPressed: () => editPrinter(printer),
                           icon: const Icon(Icons.edit),
                         ),
                       ),
