584db0f3e69a3c7c7f759b628862fbb3e986917f
Bruno Almeida de Matos <brunoalmeidadem@gmail.com>
2023-12-22 19:31:23 -0300

    EditPrintersDialog

--- contadores/agora-vai/app/lib/src/navigation/features/printers/components/edit_printers_dialog.dart
+++ contadores/agora-vai/app/lib/src/navigation/features/printers/components/edit_printers_dialog.dart
@@ -1,13 +1,50 @@
+import 'package:app/main.dart';
+import 'package:app/src/navigation/features/printers/components/edit_printers_text_field.dart';
+import 'package:app/src/navigation/features/printers/printers_controller.dart';
+import 'package:app/src/navigation/features/printers/printers_entity.dart';
 import 'package:flutter/material.dart';
 
 class EditPrintersDialog extends StatefulWidget {
-  const EditPrintersDialog({super.key});
+  final PrintersEntity printer;
+  const EditPrintersDialog({super.key, required this.printer});
 
   @override
   State<EditPrintersDialog> createState() => _EditPrintersDialogState();
 }
 
 class _EditPrintersDialogState extends State<EditPrintersDialog> {
+  final PrintersController _printersCt = di();
+  late TextEditingController pNameCT;
+  late TextEditingController pModelCT;
+  late TextEditingController pSelbCT;
+  late TextEditingController pDepCT;
+  late TextEditingController pStatusCT;
+  late TextEditingController pTonerCT;
+
+  @override
+  void initState() {
+    var printer = widget.printer;
+    pNameCT = TextEditingController(text: printer.name);
+    pModelCT = TextEditingController(text: printer.model);
+    pSelbCT = TextEditingController(text: printer.selb);
+    pDepCT = TextEditingController(text: printer.department);
+    pStatusCT = TextEditingController(text: printer.status);
+    pTonerCT = TextEditingController(text: printer.tonerLevel);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    pNameCT.dispose();
+    pModelCT.dispose();
+    pSelbCT.dispose();
+    pDepCT.dispose();
+    pStatusCT.dispose();
+    pTonerCT.dispose();
+
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return SimpleDialog(
@@ -16,13 +53,21 @@ class _EditPrintersDialogState extends State<EditPrintersDialog> {
         Padding(
           padding: const EdgeInsets.all(18.0),
           child: Form(
-            child: TextFormField(
-              decoration: const InputDecoration(
-                enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(20))),
-                focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(20))),
-              ),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                EditPrintersTextField(ct: pNameCT, text: 'Nome'),
+                const SizedBox(height: 10),
+                EditPrintersTextField(ct: pModelCT, text: 'Modelo'),
+                const SizedBox(height: 10),
+                EditPrintersTextField(ct: pSelbCT, text: 'SELB'),
+                const SizedBox(height: 10),
+                EditPrintersTextField(ct: pDepCT, text: 'Departamento'),
+                const SizedBox(height: 10),
+                EditPrintersTextField(ct: pStatusCT, text: 'Status'),
+                const SizedBox(height: 10),
+                EditPrintersTextField(ct: pTonerCT, text: 'Nível de toner'),
+              ],
             ),
           ),
         ),
@@ -33,6 +78,16 @@ class _EditPrintersDialogState extends State<EditPrintersDialog> {
             children: [
               ElevatedButton(
                 onPressed: () {
+                  var printer = widget.printer;
+                  PrintersEntity newPrinter = printer.copyWith(
+                    name: pNameCT.text.trim(),
+                    department: pDepCT.text.trim(),
+                    tonerLevel: pTonerCT.text.trim(),
+                    selb: pSelbCT.text.trim(),
+                    model: pModelCT.text.trim(),
+                    status: pStatusCT.text.trim(),
+                  );
+                  _printersCt.updatePrinters(newPrinter);
                   Navigator.of(context).pop();
                 },
                 style: ElevatedButton.styleFrom(
