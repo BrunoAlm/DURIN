a59060c77e317274a5b01667fbfab4b7bb06034a
Bruno Almeida de Matos <brunoalmeidadem@gmail.com>
2023-12-22 19:29:29 -0300

    Update home_page.dart

--- contadores/agora-vai/app/lib/src/navigation/features/home/home_page.dart
+++ contadores/agora-vai/app/lib/src/navigation/features/home/home_page.dart
@@ -6,7 +6,7 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const SizedBox(
-      child: Text('Home Page Teste'),
+      child: Text('Home Page'),
     );
   }
 }
