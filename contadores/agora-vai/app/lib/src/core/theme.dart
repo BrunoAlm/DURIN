1a148ab8866a3e3f8b1ff21f3ece906a18905194
Bruno Almeida de Matos <brunoalmeidadem@gmail.com>
2023-12-24 03:14:45 -0300

    Delete theme.dart

--- contadores/agora-vai/app/lib/src/core/theme.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-class CustomTextTheme {
-  static  TextStyle titleStyle = const TextStyle(
-      fontWeight: FontWeight.bold,
-      fontSize: 22,
-  );
-}
